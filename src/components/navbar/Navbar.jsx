e3f6a2a8b60b2d4727c7cfc4bb738a78122860ca
bazimR <rishaalmuhammed@gmail.com>
2023-06-28 10:39:50 +0530

    UPDATE

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ flexGrow: 1 }} >
+      <AppBar position="static"elevation={0}>
         <Toolbar>
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 ,marginLeft:2}}>
             Sales entry
